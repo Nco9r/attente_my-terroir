f600b22cf3718b733c7cacbf5311b5415e205dd6
Nco9r <Nicolas929@me.com>
2021-04-14 11:56:21 +0200

    first_commit

---
+++ pages/index.vue
@@ -0,0 +1,374 @@
+<template>
+  <div class="attente">
+   <div class="block_description">
+     <div class="cover_block">
+       <div class="logo">
+         <img src="@/assets/logo_MT.svg" alt="">
+       </div>
+       <div class="title_mt">
+         <h1>traiteur, fromages, charcuteries et viandes de producteurs <span class="orange">!</span>  </h1>
+       </div>
+       <div class="content_block">
+         <p>Notre secret : de <strong>bons produits</strong> achetés directement aux producteurs <strong>(fromages , viandes et charcuteries)</strong>,  une équipe passionnée, motivée et quelques bons livres de grand-mères.  Bon on avoue : il nous aura fallu 5 ans, <strong>beaucoup de travail</strong>, de nombreux voyages et autant de rencontres pour pouvoir vous proposer en toute humilité <strong>notre expertise en la matière</strong>. Pourquoi chercher compliqué quand nous pouvons <strong>vous régaler</strong> en toute simplicité!</p>
+       </div>
+       <div class="slogan">
+         <p>Le <strong>vrai goût</strong> des choses <span class="orange">!</span></p>
+       </div>
+     </div>
+   </div>
+   <div class="block_maintenance">
+     <div class="block_center">
+     <div class="stickers">
+       <img src="@/assets/ronds.png" alt="">
+     </div>
+     <div class="content_maintenance">
+       <div class="title_deg">
+         <h2><strong>maintenance</strong> du site</h2>
+       </div>
+       <div class="content">
+         <h3><strong>Refonte</strong> du site</h3>
+         <p>Nous sommes en refonte de notre site internet pour vous apporter encore plus de contenu. En attendant vous pouvez continuer à nous suivre sur nos différents réseaux sociaux (Facebook et Instagram).  </p>
+         <p>Vous pouvez également nous joindre  par téléphone ou E-mail :  </p>
+         <ul>
+           <li>
+             06 24 98 83 00 
+           </li>
+           <li>
+             contact@my-terroir.fr
+           </li>
+         </ul>
+         <p>À bientôt chez <strong>My-Terroir</strong> <span class="orange">!</span> </p>
+         <div class="btn_menu">
+              <a href="https://www.instagram.com/myterroir/" target="_blank"><button> <svg xmlns="http://www.w3.org/2000/svg" width="23.242" height="16.215" viewBox="0 0 23.242 16.215">
+                <path id="Tracé_1781" data-name="Tracé 1781" d="M-15.034,32.87c.209-4.165,2.718-6.645,6.452-6.97V23.6c-3.734-.325-6.244-2.777-6.452-6.941l-2.779.591a7.99,7.99,0,0,0,1.584,4.046,6.322,6.322,0,0,0,3.166,2.3H-31.823v2.3h18.761a6.21,6.21,0,0,0-3.166,2.333,7.994,7.994,0,0,0-1.584,4.017Z" transform="translate(31.823 -16.655)" fill="#fff"/>
+                </svg>Cliquez ici</button></a>
+          </div>
+       </div>
+     </div>
+     <div class="copyright">
+       <p><strong>©2021</strong> - My_Terroir - Conception <strong><a href="https://rouxnicolas.fr" target="_blank">Nicolas ROUX.</a></strong></p>
+       <div class="icon_sociaux">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="8.434" height="18.359" viewBox="0 0 8.434 18.359">
+                            <path id="facebook_3_" data-name="facebook (3)" d="M13.082,3.048h1.54V.129A18.288,18.288,0,0,0,12.378,0c-2.22,0-3.741,1.52-3.741,4.314V6.885H6.187v3.263h2.45v8.211h3v-8.21h2.351l.373-3.263H11.64V4.637C11.641,3.694,11.874,3.048,13.082,3.048Z" transform="translate(-6.187)" fill="#fff"/>
+                        </svg>
+                        <svg id="instagram_2_" data-name="instagram (2)" xmlns="http://www.w3.org/2000/svg" width="17.179" height="17.183" viewBox="0 0 17.179 17.183">
+                            <path id="Tracé_1502" data-name="Tracé 1502" d="M17.585,5.052a6.279,6.279,0,0,0-.4-2.085A4.4,4.4,0,0,0,14.671.453a6.3,6.3,0,0,0-2.084-.4C11.667.01,11.375,0,9.042,0S6.417.01,5.5.05a6.28,6.28,0,0,0-2.084.4,4.192,4.192,0,0,0-1.524.994A4.23,4.23,0,0,0,.9,2.964,6.3,6.3,0,0,0,.5,5.048C.459,5.968.449,6.26.449,8.593s.01,2.625.05,3.541a6.278,6.278,0,0,0,.4,2.085,4.4,4.4,0,0,0,2.514,2.514,6.3,6.3,0,0,0,2.085.4c.916.04,1.208.05,3.541.05s2.625-.01,3.541-.05a6.277,6.277,0,0,0,2.084-.4,4.395,4.395,0,0,0,2.514-2.514,6.3,6.3,0,0,0,.4-2.085c.04-.916.05-1.208.05-3.541S17.625,5.968,17.585,5.052Zm-1.547,7.015a4.71,4.71,0,0,1-.3,1.594,2.85,2.85,0,0,1-1.631,1.631,4.726,4.726,0,0,1-1.594.3c-.906.04-1.178.05-3.471.05s-2.568-.01-3.471-.05a4.707,4.707,0,0,1-1.594-.3,2.644,2.644,0,0,1-.987-.641,2.671,2.671,0,0,1-.641-.987,4.727,4.727,0,0,1-.3-1.594c-.04-.906-.05-1.178-.05-3.471s.01-2.568.05-3.471a4.707,4.707,0,0,1,.3-1.594A2.612,2.612,0,0,1,3,2.548a2.667,2.667,0,0,1,.987-.641,4.73,4.73,0,0,1,1.594-.3c.906-.04,1.178-.05,3.471-.05s2.568.01,3.471.05a4.71,4.71,0,0,1,1.594.3,2.642,2.642,0,0,1,.987.641,2.671,2.671,0,0,1,.641.987,4.729,4.729,0,0,1,.3,1.594c.04.906.05,1.178.05,3.471S16.078,11.161,16.037,12.067Zm0,0" transform="translate(-0.449 0)" fill="#fff"/>
+                            <path id="Tracé_1503" data-name="Tracé 1503" d="M129.363,124.5a4.414,4.414,0,1,0,4.414,4.414A4.415,4.415,0,0,0,129.363,124.5Zm0,7.277a2.863,2.863,0,1,1,2.863-2.863A2.864,2.864,0,0,1,129.363,131.777Zm0,0" transform="translate(-120.77 -120.321)" fill="#fff"/>
+                            <path id="Tracé_1504" data-name="Tracé 1504" d="M364.51,89.632a1.031,1.031,0,1,1-1.031-1.03A1.031,1.031,0,0,1,364.51,89.632Zm0,0" transform="translate(-350.298 -85.628)" fill="#fff"/>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30.983" height="18.359" viewBox="0 0 30.983 18.359">
+                            <g id="tripadvisor-logotype" transform="translate(0)">
+                                <g id="Groupe_29" data-name="Groupe 29" transform="translate(0 0)">
+                                <path id="Tracé_1505" data-name="Tracé 1505" d="M18.987,20.369a16.611,16.611,0,0,1,2.548.6,17.438,17.438,0,0,1,4.135,1.885.634.634,0,0,0,.317.091c1.523.006,3.045,0,4.566.005.123,0,.245.013.367.02,0,.03,0,.042,0,.049-.071.109-.143.217-.214.326a9.34,9.34,0,0,0-1.218,2.537.292.292,0,0,0,.032.277A7.766,7.766,0,0,1,23.2,38.441a7.21,7.21,0,0,1-1.853-.238,7.773,7.773,0,0,1-3.805-2.246c-.138-.147-.268-.3-.416-.467l-1.659,2.474L13.83,35.525a.414.414,0,0,0-.055.029.253.253,0,0,0-.032.034A7.691,7.691,0,0,1,8.934,38.31a7.512,7.512,0,0,1-3.2-.18A7.774,7.774,0,0,1,.1,31.834a7.5,7.5,0,0,1,.139-3.068,7.8,7.8,0,0,1,1.208-2.623.3.3,0,0,0,.035-.225A8.668,8.668,0,0,0,.4,23.578c-.118-.186-.258-.359-.389-.538v-.062a.66.66,0,0,0,.077.008q2.337,0,4.673,0a.4.4,0,0,0,.2-.065,16.785,16.785,0,0,1,3.486-1.692,18.944,18.944,0,0,1,2.706-.733,21.676,21.676,0,0,1,2.691-.353A22.834,22.834,0,0,1,18.987,20.369ZM17.034,30.658A6.183,6.183,0,1,0,23.085,24.5,6.192,6.192,0,0,0,17.034,30.658ZM7.757,24.476a6.176,6.176,0,1,0,6.19,6.2A6.183,6.183,0,0,0,7.757,24.476Zm.307-1.614a7.889,7.889,0,0,1,7.43,6.742A7.86,7.86,0,0,1,22.806,22.9a17.587,17.587,0,0,0-6.889-1.436A19.077,19.077,0,0,0,8.064,22.862Z" transform="translate(0 -20.082)" fill="#fff"/>
+                                <path id="Tracé_1506" data-name="Tracé 1506" d="M65.447,41.31a3.83,3.83,0,1,1-3.839,3.826A3.817,3.817,0,0,1,65.447,41.31Zm2.5,3.826a2.5,2.5,0,0,0-5.007-.01,2.5,2.5,0,1,0,5.007.01Z" transform="translate(-42.242 -34.637)" fill="#fff"/>
+                                <path id="Tracé_1507" data-name="Tracé 1507" d="M16.033,41.31a3.83,3.83,0,1,1-3.843,3.822A3.816,3.816,0,0,1,16.033,41.31Zm2.494,3.818a2.505,2.505,0,0,0-5.01.008,2.505,2.505,0,1,0,5.01-.008Z" transform="translate(-8.358 -34.637)" fill="#fff"/>
+                                <path id="Tracé_1508" data-name="Tracé 1508" d="M71.053,49.443a1.269,1.269,0,1,1-1.272,1.281A1.27,1.27,0,0,1,71.053,49.443Z" transform="translate(-47.846 -40.214)" fill="#fff"/>
+                                <path id="Tracé_1509" data-name="Tracé 1509" d="M21.6,49.407a1.281,1.281,0,1,1,0,2.562,1.281,1.281,0,1,1,0-2.562Z" transform="translate(-13.933 -40.189)" fill="#fff"/>
+                                </g>
+                            </g>
+                            </svg>
+                        </div>
+     </div>
+   </div>
+   </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+   
+  }
+}
+</script>
+
+<style>
+
+a {
+  text-decoration: none; 
+  color: white;
+}
+
+.orange {
+  color: var(--orange);
+}
+
+.attente {
+  display: flex; 
+  flex-flow: column; 
+}
+
+.block_description {
+  background-image: url('~assets/pig.jpg');
+  width: 100%; 
+  height: 100vh;
+  background-position: -400px ;
+  background-size: cover;
+  background-repeat: no-repeat;
+  display: flex; 
+}
+
+.cover_block {
+  width: 340px; 
+  background-color: white;
+  display: flex; 
+  flex-flow: column;
+  margin: auto;
+  padding: 20px;
+}
+
+.logo {
+  margin-top: -70px;
+  text-align: center;
+}
+
+.logo img{
+  width: 150px;
+}
+
+.title_mt h1{
+  font-size: 26px;
+  margin-bottom: 15px;
+}
+
+.content_block p {
+  font-family: 'Bebas-neue-pro-expanded', sans-serif;
+  line-height: 24px;
+  margin-bottom: 15px;
+}
+
+.slogan p{
+  font-size: 26px;
+  margin-bottom: 15px;
+  font-family: bebas-neue-pro, sans-serif; 
+  color: var(--black);
+  text-transform: uppercase;
+  margin-top: 10px;
+}
+
+.block_maintenance {
+  background-color: var(--black);
+  width: 100%;
+  position: relative;
+}
+
+.stickers {
+  position: absolute;
+  right: 10px;
+  top: -70px;
+  animation: move 10s infinite linear;
+    }
+
+  @keyframes move {
+    from{
+      transform: rotate(0)
+    }
+    to{
+      transform: rotate(-360deg)
+    }
+  }
+
+.stickers img {
+  width: 150px;
+}
+
+.content_maintenance {
+  display: flex; 
+  flex-flow: column;
+  padding: 20px;
+}
+
+.title_deg {
+  display: flex; 
+  flex-flow: nowrap;
+  margin-top: 70px;
+}
+
+.title_deg h2{
+  color: white; 
+  font-weight: lighter;
+  font-size: 50px;
+  display: f
+}
+
+.content {
+  margin-top: 15px;
+
+}
+
+.content h3 {
+  color: white;
+  font-size: 28px;
+  font-weight: lighter;
+  margin-bottom: 15px;
+  display: none;
+
+}
+
+.content p {
+  color: white;
+  font-family: 'Bebas-neue-pro-expanded', sans-serif;
+  line-height: 24px;
+  margin-bottom: 15px;
+}
+
+ul {
+  color: var(--orange);
+  font-weight: bold; 
+  font-family: 'Bebas-neue-pro', sans-serif;
+  line-height: 24px;
+  font-size: 20px;
+  margin-bottom: 20px;
+
+}
+
+ul li {
+  margin-bottom: 10px;
+}
+
+.btn_menu a{
+  display: flex; 
+  margin:  auto;
+  text-align: center;
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+
+.btn_menu a button  {
+    padding: 8px 16px; 
+    width: 250px;
+    margin:  auto;
+    font-family: bebas-neue-pro, sans-serif; 
+    background-color: var(--orange);
+    box-shadow: 4px 4px rgba(218, 71, 9, 0.486);
+    border: none;
+    text-decoration: none;
+    color: white;
+    font-size: 20px;
+    font-weight: bold; 
+    border: 30px var(--orange);
+    text-transform: uppercase;
+    background-size: 60%;
+    outline: none; 
+    cursor: pointer;
+}
+
+.btn_menu button svg {
+    margin-right: 10px;
+}
+
+.copyright {
+  padding: 10px 20px;
+  display: flex;
+  flex-flow: column-reverse;
+}
+
+.copyright p {
+  color: white;
+  font-family: 'Bebas-neue-pro-expanded', sans-serif;
+  line-height: 24px;
+}
+
+.icon_sociaux {
+  margin-bottom: 10px;
+}
+
+.icon_sociaux svg{
+  margin-right: 10px;
+}
+
+
+
+
+@media screen and (min-width: 768px) {
+  .cover_block {
+    width: 640px; 
+    background-color: white;
+    display: flex; 
+    flex-flow: column;
+    margin: auto;
+    padding: 40px;
+  }
+
+  .block_description {
+  background-image: url('~assets/pig.jpg');
+  background-position: 0px;
+  background-size: 100%;
+  background-size: cover;
+
+}
+
+   .block_maintenance {
+    padding: 20px 100px 10px 100px;
+    display: flex;
+    flex-flow: column;
+  }
+
+  .block_center {
+    display: flex;
+    flex-flow: column;
+    margin: auto;
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  .attente {
+    display: flex; 
+    flex-flow: row; 
+  }
+
+  .cover_block {
+    width: 440px; 
+    padding: 20px;
+
+  }
+
+  .block_description {
+    width: 50%;
+  
+  }
+
+  .block_maintenance {
+    width: 50%;
+    padding: 20px 50px 10px 50px;
+  
+  }
+
+  .stickers {
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    animation: move 15s infinite linear;
+    }
+
+  @keyframes move {
+    from{
+      transform: rotate(0)
+    }
+    to{
+      transform: rotate(-360deg)
+    }
+  }
+
+  .stickers img {
+    width: 110px;
+  }
+
+  .copyright {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+  }
+  
+}
+
+@media screen and (min-width: 1440px) {
+ .block_maintenance {
+ 
+    padding: 20px 150px 10px 150px;
+  }
+}
+
+</style>
